fb282b49014d8b517ecc174fe4fc87d50a09bb3b
Alan Murphy <32366311+Alan-Murphy91@users.noreply.github.com>
2018-07-07 18:55:36 +0100

    Add files via upload

---
+++ #5.py
@@ -0,0 +1,23 @@
+'''
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns
+the first and last element of that pair.
+For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+'''
+
+def cons(a,b):
+    def pair(f):
+        return f(a,b)
+    return pair
+
+def car(pair):
+    return pair(lambda a,b: a)
+
+def cdr(pair):
+    return pair(lambda a,b: b)
+
+'''
+Lambda functions can't act on a tuple or a list. Cons is a closure, we can
+only return the two individual arguments fed to it as a pair by returning them as
+the arguments of a locally declared anonymous function. Lambda can then act
+on this result.
+'''
