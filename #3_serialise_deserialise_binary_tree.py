6de987b76e1f3eb40a42e8136d8b120bad4439bb
Alan Murphy <32366311+Alan-Murphy91@users.noreply.github.com>
2018-08-18 17:53:19 +0100

    Add files via upload

--- #3_serialise_deserialise_binary_tree.py
+++ #3_serialise_deserialise_binary_tree.py
@@ -23,5 +23,14 @@ def deserialize(root):
     node_iter = iter(root.split())
     return gen_nodes()
 
+    
+node = Node(1, Node(2,Node(3)), Node(4)) 
 
+
+'''
+define a node class and an instance of node in the above convention.
+serialize the node using python's format method.
+define a class which steps through the iteration of the serialised class and performs a
+recursion-based assignment of left and right nodes
+'''
 
